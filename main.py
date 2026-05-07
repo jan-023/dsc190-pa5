5930fdd8148d2aca2f931f82ddbf28f5291f275e
Jacqueline Ng <jan023@ucsd.edu>
2026-05-06 19:27:25 -0700

    Modified main.py to contain ruff rule violations

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 def main():
     print("Hello from dsc190-pa5!")
+    
+    if len("lint violation 1"): # lint violation: len({expression}) used as condition w/o comparison
+        print("Passed lint violation 1 conditional")
+    
+    línt_violation_2 = 2 # lint violation: non-ASCII char used in variable name
 
+import numpy # lint violation: all import statements at top of file
 
 if __name__ == "__main__":
     main()
